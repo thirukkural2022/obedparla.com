29f6bd2f963be10813f37f7316422fd432c2f0a3
Obed <obed.imp@gmail.com>
2019-12-14 19:11:05 +0100

    Fix index page due to blog data changes

--- src/templates/listing.jsx
+++ src/templates/listing.jsx
@@ -1,57 +1,59 @@
-import React from "react";
-import Helmet from "react-helmet";
-import { graphql, Link } from "gatsby";
-import Layout from "../layout/Layout";
+import React from 'react';
+import Helmet from 'react-helmet';
+import { graphql, Link } from 'gatsby';
+import Layout from '../layout/Layout';
 
-import PostListing from "../components/PostListing/PostListing";
-import SEO from "../components/SEO/SEO";
-import config from "../../data/SiteConfig";
-import "./listing.css";
+import PostListing from '../components/PostListing/PostListing';
+import SEO from '../components/SEO/SEO';
+import config from '../../data/SiteConfig';
+import './listing.css';
+import { usePostFields } from '../hooks/usePostFields';
 
-class Listing extends React.Component {
-  renderPaging() {
-    const { currentPageNum, pageCount } = this.props.pageContext;
-    const prevPage = currentPageNum - 1 === 1 ? "/" : `/${currentPageNum - 1}/`;
-    const nextPage = `/${currentPageNum + 1}/`;
-    const isFirstPage = currentPageNum === 1;
-    const isLastPage = currentPageNum === pageCount;
+const renderPaging = props => {
+  const { currentPageNum, pageCount } = props.pageContext;
+  const prevPage = currentPageNum - 1 === 1 ? '/' : `/${currentPageNum - 1}/`;
+  const nextPage = `/${currentPageNum + 1}/`;
+  const isFirstPage = currentPageNum === 1;
+  const isLastPage = currentPageNum === pageCount;
 
-    return (
-      <div className="paging-container">
-        {!isFirstPage && <Link to={prevPage}>Previous</Link>}
-        {[...Array(pageCount)].map((_val, index) => {
-          const pageNum = index + 1;
-          return (
-            <Link
-              key={`listing-page-${pageNum}`}
-              to={pageNum === 1 ? "/" : `/${pageNum}/`}
-            >
-              {pageNum}
-            </Link>
-          );
-        })}
-        {!isLastPage && <Link to={nextPage}>Next</Link>}
-      </div>
-    );
-  }
+  return (
+    <div className='paging-container'>
+      {!isFirstPage && <Link to={prevPage}>Previous</Link>}
+      {[...Array(pageCount)].map((_val, index) => {
+        const pageNum = index + 1;
+        return (
+          <Link
+            key={`listing-page-${pageNum}`}
+            to={pageNum === 1 ? '/' : `/${pageNum}/`}
+          >
+            {pageNum}
+          </Link>
+        );
+      })}
+      {!isLastPage && <Link to={nextPage}>Next</Link>}
+    </div>
+  );
+};
 
-  render() {
-    const postEdges = this.props.data.allMarkdownRemark.edges;
+const Listing = props => {
+  console.log('props.data', props.data);
+  const postEdges = props.data.allMarkdownRemark.edges;
+  console.log('postEdges', postEdges);
+  const postFields = usePostFields(postEdges);
 
-    return (
-      <Layout>
-        <div className="listing-container">
-          <div className="posts-container">
-            <Helmet title={config.siteTitle} />
-            <SEO />
-            <PostListing postEdges={postEdges} />
-          </div>
-          {this.renderPaging()}
+  return (
+    <Layout>
+      <div className='listing-container'>
+        <div className='posts-container'>
+          <Helmet title={config.siteTitle} />
+          <SEO />
+          <PostListing postList={postFields} />
         </div>
-      </Layout>
-    );
-  }
-}
+        {renderPaging(props)}
+      </div>
+    </Layout>
+  );
+};
 
 export default Listing;
 
