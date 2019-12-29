f82db93c83bff47a2cbc637947a486077893d8e7
Obed <obed.imp@gmail.com>
2019-12-29 09:46:49 +0100

    Fix social links styles

--- src/components/SocialLinks/styles.js
+++ src/components/SocialLinks/styles.js
@@ -1,23 +1,21 @@
 import styled from '@emotion/styled';
 
 export const StyledSocialLinks = styled.div`
-  .social-links {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-content: center;
-    align-items: center;
-    margin: 8px 0;
-    border-top: 1px solid #ccc;
-    padding-top: 1.56rem;
-  }
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  margin: 8px 0;
+  border-top: 1px solid #ccc;
+  padding-top: 1.56rem;
 
-  .social-links > div {
+  & > div {
     cursor: pointer;
   }
 
-  .social-links svg {
+  & svg {
     height: 36px;
     width: 36px;
   }
