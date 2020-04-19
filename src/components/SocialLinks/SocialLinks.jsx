5747d35be8d27f89653b047fd8c65170955a67c2
Obed <obed.imp@gmail.com>
2020-04-19 19:30:40 +0200

    Fix social links, commited by mistake lol

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -1,29 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  LinkedinIcon,
+  RedditShareButton,
+  RedditIcon,
+} from 'react-share';
+import urljoin from 'url-join';
+import config from '../../../data/SiteConfig';
+import { StyledSocialLinks } from './styles';
 
-const Li = props => <li>{props.children}</li>;
-export const PotatoBeans = () => (
-  <div>
-    <div>
-      <p>Hello</p>
-    </div>
-    <ul>
-      <Li> Never</Li>
-      <Li color={'red'}>gonna</Li>
-      <Li>let</Li>
-      <Li highlight>you</Li>
-      <Li>down</Li>
-    </ul>
-  </div>
-);
+class SocialLinks extends Component {
+  render() {
+    const { postNode, postPath, mobile } = this.props;
+    const post = postNode.frontmatter;
+    const url = urljoin(config.siteUrl, config.pathPrefix, postPath);
+    const iconSize = mobile ? 36 : 48;
 
-const A = () => {
-  const touchThis = 'awww';
+    return (
+      <StyledSocialLinks>
+        <TwitterShareButton url={url} title={post.title}>
+          <TwitterIcon round size={iconSize} />
+        </TwitterShareButton>
+        <FacebookShareButton url={url} quote={postNode.excerpt}>
+          <FacebookIcon round size={iconSize} />
+        </FacebookShareButton>
+        <LinkedinShareButton
+          url={url}
+          title={post.title}
+          description={postNode.excerpt}
+        >
+          <LinkedinIcon round size={iconSize} />
+        </LinkedinShareButton>
+        <RedditShareButton url={url} title={post.title}>
+          <RedditIcon round size={iconSize} />
+        </RedditShareButton>
+      </StyledSocialLinks>
+    );
+  }
+}
 
-  const B = () => {
-    const cantTouchThis = "can't touch this";
-
-    console.log(`${touchThis} ${cantTouchThis}`); // awww can't touch this
-  };
-
-  console.log(cantTouchThis); // Error
-};
+export default SocialLinks;
